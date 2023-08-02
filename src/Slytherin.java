284b5dfa05382ffc35929389480ea75854ac3419
Romchig <romulusto@gmail.com>
2023-08-03 00:00:09 +0200

    После исправлений

--- src/Slytherin.java
+++ src/Slytherin.java
@@ -4,14 +4,28 @@ public class Slytherin extends Hogwarts {
     private int ambition;
     private int resourcefulness;
     private int lustForPower;
+    private final int ability;
 
-    public Slytherin(String name,String surname,int magicPower, int transgressionDistance, int resourcefulness, int lustForPower,int cunning,int determination,int ambition) {
-        super(magicPower, transgressionDistance,name,surname);
+    public void compareSlytherinStudents(Slytherin y) {
+        if (y.getAbility() == this.getAbility()) {
+            System.out.println(y.getName() + " " + "и " + this.getName() + " одинаковые слизеринцы");
+        } else if (y.getAbility() > this.getAbility()) {
+            System.out.println(y.getName() + " " + " лучший слизеринец чем " + this.getName());
+        } else {
+            System.out.println(this.getName() + " " + " лучший слизеринец чем " + y.getName());
+
+        }
+
+    }
+
+    public Slytherin(String name, String surname, int magicPower, int transgressionDistance, int resourcefulness, int lustForPower, int cunning, int determination, int ambition) {
+        super(magicPower, transgressionDistance, name, surname);
         this.cunning = cunning;
         this.determination = determination;
         this.ambition = ambition;
         this.resourcefulness = resourcefulness;
         this.lustForPower = lustForPower;
+        this.ability = this.cunning + this.determination + this.ambition + this.resourcefulness + this.lustForPower;
     }
 
     public int getCunning() {
@@ -54,11 +68,15 @@ public class Slytherin extends Hogwarts {
         this.lustForPower = lustForPower;
     }
 
+    public int getAbility() {
+        return ability;
+    }
+
     @Override
     public String toString() {
         return "Slytherin " + "name= " + getName() + " " +
                 "surname= " + getSurname() + " " + "magicPower= " + getMagicPower() + " " +
-                "transgressionDistance= " + getTransgressionDistance()+ " " +
+                "transgressionDistance= " + getTransgressionDistance() + " " +
                 "cunning= " + cunning + " " +
                 "determination= " + determination + " " +
                 "ambition= " + ambition + " " +
